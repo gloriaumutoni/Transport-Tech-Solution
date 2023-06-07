e81be19e5d63ff44bebe5e15775e7c6c283e9f95
ishimweaxcel <aishimwe811@gmail.com>
2023-06-07 15:55:33 +0200

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client'
 // import App from './App.jsx'
  
 import './index.css'
-// import Contact from '../src/pages/contact-us/contactUs'
-import BookingForm from './pages/Booking/BookingForm'
+import Contact from '../src/pages/contact-us/contactUs'
+// import BookingForm from './pages/Booking/BookingForm'
 
 // import DropDown from '../src/pages/DropDown/DropDown'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BookingForm />
+    <Contact />
   </React.StrictMode>,
 )
