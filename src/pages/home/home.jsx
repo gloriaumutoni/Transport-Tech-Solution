ab2ddd3192c40e9b91f5a6762fbc48db513bfd80
Your name <you@example.com>
2023-06-14 16:38:36 +0200

    fixing deployment on homepage

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -4,7 +4,7 @@ import NavBar  from "../../pages/navigation/NavigationBar.jsx";
 import { FaSearch } from 'react-icons/fa';
 import {AiOutlineSwapRight} from 'react-icons/ai'
 import {AiOutlineRight} from 'react-icons/ai'
-// import {motion} from 'framer-motion'
+import {motion} from 'framer-motion'
 import { useRef,useEffect,useState } from 'react'
 import images from './images'
 import Image from '../image/ElecMobility.jpg'
@@ -17,20 +17,20 @@ const Home =()=>{
   setWidth(carousel.current - carousel.current)
     },[])
     return(
-        <div className="whole1">
-            <NavBar/>
-        <div className="container1">
+        <div className="whole1" id="home-fix-one">
+            {/* <NavBar/> */}
+        <div className="home-container1">
 <img src={Image}/>
 <div className="overlay"></div>
 <div className="whole2"> 
-<h1>Your next stop awaits</h1> 
+<h1 id="home-fix-header">Your next stop awaits</h1> 
 <button>Book here</button>
 </div>
         </div>
         <div className="container2">
             <div className="cont2">
-            <h2>Find schedule</h2> 
-       <p>Quickly look up scheduled times for a specific bus stop, bus route or station</p>
+            <h2 id="home-fix-header">Find schedule</h2> 
+         <p id="home-fix-para">Quickly look up scheduled times for a specific bus stop, bus route or station</p>
        <form>
         <div className="input">
         <div className="input1">
@@ -52,15 +52,15 @@ const Home =()=>{
         </div>
 <div className="whole3">
     <div className="one">
-<p>Schedule and maps</p>
+  <p id="home-fix-para">Schedule and maps</p>
     </div>
     <div className="two">
-    <p> bus schedules</p>
+      <p id="home-fix-para"> bus schedules</p>
     </div>
 </div>
 
 <div className="schedule">
-    <h1>Bus Schedule</h1>
+    <h1 id="home-fix-header">Bus Schedule</h1>
     <p className="p">Our three automated rapid transit Bus lines take you all around Kigali:</p>
     <div className="p1">
         <p className="p2">  Gasabo district  <AiOutlineSwapRight/> 34 bus routes </p>
@@ -85,7 +85,7 @@ const Home =()=>{
         </div>
         <div className="rect">
             <h4>Discover all destinations</h4>
-            <p>Choose from over 100 routes destination in Kigali</p>
+              <p id="home-fix-para">Choose from over 100 routes destination in Kigali</p>
             <button>Expore the map <AiOutlineRight style={{paddingTop:'2%'}}/></button>
         </div>
     </div>
@@ -94,22 +94,22 @@ const Home =()=>{
 </div>
 <div className="paragraph">
     <div className="parg1">
-        <h2>Get Cheap Bus Tickets Easily with Us</h2>
-        <p>If you're looking to travel by bus , We got you covered with our extensive bus routes! With over 100 routes all over Kigali, you can easily find the perfect option . Plus, our cheap bus tickets make it easy and affordable to get whenever you want to manage your time properly, whether it's for work or play from station to staion.</p>
+        <h2 id="home-fix-header">Get Cheap Bus Tickets Easily with Us</h2>
+          <p id="home-fix-para">If you're looking to travel by bus , We got you covered with our extensive bus routes! With over 100 routes all over Kigali, you can easily find the perfect option . Plus, our cheap bus tickets make it easy and affordable to get whenever you want to manage your time properly, whether it's for work or play from station to staion.</p>
     </div>
     <div  className="parg2">
-<h2>Bus Routes to Suit Your Schedule</h2>
-<p>Boarding with us is simple and hassle-free, thanks to our frequent bus connections, reliable schedules, and modern vehicles. With over 400 buses from different stations, including those from kicukiro and nyarugenge, you can easily find the perfect option for your location. Additionally, We offer bus travel in different province, providing even more travel options to suit your needs. Our extensive network spans to 4 provinces  ,making it easy to plan and book your trip online or through our user-friendly website. Plus, stay connected on the go with free WiFi on most of our buses , and track your journey in real-time with our advanced tracking system.</p>
+<h2 id="home-fix-header">Bus Routes to Suit Your Schedule</h2>
+  <p id="home-fix-para">Boarding with us is simple and hassle-free, thanks to our frequent bus connections, reliable schedules, and modern vehicles. With over 400 buses from different stations, including those from kicukiro and nyarugenge, you can easily find the perfect option for your location. Additionally, We offer bus travel in different province, providing even more travel options to suit your needs. Our extensive network spans to 4 provinces  ,making it easy to plan and book your trip online or through our user-friendly website. Plus, stay connected on the go with free WiFi on most of our buses , and track your journey in real-time with our advanced tracking system.</p>
     </div>
     <div  className="parg3">
-        <h2>Book your seat with us</h2>
-        <p>Our website makes it easy to plan your journey, find the cheapest bus fares, and purchase your bus tickets securely. Whether you are planning or booking last-minute trip, We make buying bus tickets a breeze. Plus, with our convenient e-ticket system, you can board your bus without the hassle of paper tickets as your phone will serve as your ticket.</p>
+        <h2 id="home-fix-header">Book your seat with us</h2>
+          <p id="home-fix-para">Our website makes it easy to plan your journey, find the cheapest bus fares, and purchase your bus tickets securely. Whether you are planning or booking last-minute trip, We make buying bus tickets a breeze. Plus, with our convenient e-ticket system, you can board your bus without the hassle of paper tickets as your phone will serve as your ticket.</p>
     </div>
 </div>
 
 <div className="drag">
-<p>Explore roads with us</p>
-<h1>Our Bus Stops</h1>
+  <p id="home-fix-para">Explore roads with us</p>
+<h1 id="home-fix-header">Our Bus Stops</h1>
 <div id='container'>
             <motion.div ref={carousel} className='carousel' whileTap={{cursor: 'grabbing'}}  >
               <motion.div drag="x" dragConstraints={{right:0,left: -width}} className='inner-carousel'>
