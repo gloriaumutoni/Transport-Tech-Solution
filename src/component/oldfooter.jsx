1c4b63dffaccaf97b8dab3e4cb00a7f78f849a33
Delphine30 <kamusabwadelphine@gmail.com>
2023-06-14 16:59:55 +0200

    new footer

---
+++ src/component/oldfooter.jsx
@@ -0,0 +1,96 @@
+// import React from 'react'
+ 
+
+// import  { AiFillLinkedin } from "react-icons/ai"; 
+// import { AiFillFacebook } from "react-icons/ai";
+// import { AiFillInstagram} from "react-icons/ai";
+// import { FaTwitter} from "react-icons/fa";
+
+
+// / import { HiLocationMarker } from "react-icons/hi";;
+// import{MdCall} from "react-icons/md";
+// import{MdEmail} from "react-icons/md";
+// import{TbWorldWww} from "react-icons/tb";/
+
+// function Footer(){
+//   return(
+   
+     
+//     <>
+//     <div className='all'>
+//       <div className='paragraph'>
+//         <div className='navbar'>
+//           <div className='nav'>
+//             <ul>
+//             <li><a href='ABOUT-US'>ABOUT-US</a></li>
+//               <li>contact</li>
+//               <li>aus</li>
+//               <li>about us</li>
+//             </ul>
+//           </div>
+//           <ul>
+//           <div className='lines-1'>
+
+//           </div>
+//             <div className='north'>
+//              {/* <footer>content</footer>  */}
+//             <li><a href='ABOUT-US'>ABOUT-US</a></li>
+//             <p>Welcome all members to<br/>
+//             join us here we provide <br/>
+//             Best services to everyone<br/>
+//             And this bus transport goes<br/>
+//             On time, for peaple who join<br/>
+//             We get more benefit to save<br/>
+//             More money and save time
+
+//             </p>
+//             </div>
+//             <div className='quick'>
+//             <li><a href='QUICK-LINK'>QUICK-LINK</a></li>
+//             <p>Home</p><br/> Blogs<br/><br/> Contacts<br/><br/> About us
+//             </div>
+//             <div className='left'>
+//             <li><a href='LET-US HELP'>LET-US HELP</a></li>
+//             <p>Your Account</p><br/>Your Location<br/><br/>Your Price<br/><br/>Manager your Time<br/>Help
+//             </div>
+//             <div className='east'>
+//             <li><a href='CONTACT'>CONTACT</a></li>
+//             <div className='cons'>
+//             <p > <HiLocationMarker style={{width:"30px"}}/>Location</p>
+//             <br/><MdCall style={{width:"30px"}} />(+250 700 000 000)<br/><br/>
+//             <MdEmail style={{width:"30px"}}/>Transport-Sln@gmail.com<br/> <br/>
+//             <TbWorldWww style={{width:"30px"}} /> www.transport-sln.com
+//             </div>
+//             </div>
+//           </ul>
+         
+//     </div>
+//     <div className='line'>
+
+//     </div>
+
+//     <div class="icons">
+//     <div className='one'>
+//         < AiFillFacebook />
+//         </div>
+//       <div className='two'>
+//         <FaTwitter/>
+//         </div>
+//         <div className='three'>
+//         < AiFillInstagram/>
+//         </div>
+//         <div className='four'>
+//           < AiFillLinkedin/>
+       
+//         </div>
+     
+//       </div>
+//  </div>
+//  <div className='down'>
+// <p>copy @ 2020 All right reserved by:Bus Link</p>
+//     </div>
+//  </div>
+//   </>
+//   )
+// }
+// export default Footer;
