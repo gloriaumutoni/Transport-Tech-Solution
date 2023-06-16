b58328f972d31bbb6534c4c02fa3bb4213252fa2
Grace <munezero.grace911@gmail.com>
2023-06-16 15:16:15 +0200

    fix three

--- src/pages/booking/Booking.jsx
+++ src/pages/booking/Booking.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import "./booking.css";
-import drivers from '../../../src/Driver'
+import drivers from "../../../src/Driver";
 import DriveSidebar from "../../component/Side/driveSidebar";
  
 const Booking = () => {
   return (
-    <div className="container">
+    <div className="driverContainer" id="booking-fix-error">
       <div className="sidebar">
-        <DriveSidebar/>
+        <DriveSidebar />
       </div>
-      <div className="content">
-        <div className="KicukiroNyabugogo">
-          <span className="loc">Kicukiro - Nyabugogo</span>
+      <div className="driverContent">
+        <div className="KicukiroNyabugogoos">
+          <span className="driverLocation">Kicukiro - Nyabugogo</span>
         </div>
-        <div className="table">
+        <div className="driverTable">
           <table>
             <thead className="table-header">
               <th className="header">No</th>
@@ -21,25 +21,25 @@ const Booking = () => {
               <th className="header">Seats Booked</th>
               <th className="header"> Booking Status</th>
             </thead>
-            <tbooking-fix-error className="table-booking-fix-error">
+            <tbody className="table-body">
               {drivers.map((item) => (
                 <tr className="table-row">
                   <td className="item0">{item.no}</td>
                   <td className="item1">{item.name}</td>
                   <td className="item2">{item.seat}</td>
                   <td className="item3">
-                    <div className="holder ">
+                    <div className="driverHolder">
                       <div className={item.accepted === false ? "box1" : ""}>
                         <span className="deny">Deny</span>
                       </div>
                       <div className={item.accepted === true ? "box2" : ""}>
-                      <span className="accept">Accept</span>
+                        <span className="accept">Accept</span>
                       </div>
                     </div>
                   </td>
                 </tr>
               ))}
-            </tbooking-fix-error>
+            </tbody>
           </table>
         </div>
       </div>
