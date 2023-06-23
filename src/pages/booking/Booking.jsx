2cffa0d01fb9bc98247b6af4c15789ca805d4e90
mugisha <mugishaelvis456@gmail.com>
2023-06-23 23:58:10 +0200

    intergration

--- src/pages/booking/Booking.jsx
+++ src/pages/booking/Booking.jsx
@@ -1,8 +1,38 @@
-import React from "react";
+import React ,{useEffect,useState}from "react";
 import "./booking.css";
 import drivers from "../../../src/Driver";
 import DriverSidebar from "../../component/Side/driveSidebar";
 const Booking = () => {
+  // const [data, setData] = useState();
+
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
+
+  // const fetchData = async () => {
+  //   try {
+  //     const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ODE5MGJlZTc3OGI3ZjNlNmNkMTAyMyIsImVtYWlsIjoibXVnaXNoYWVsdmlzNDU2QGdtYWlsLmNvbSIsImlhdCI6MTY4NjMwNjU5MH0.jwkmGiRnF_Xe06T5vuT4WCHAZ_t8AngNbYRcuqeUA0g';
+  //     const headers = {
+  //       Authorization: `Bearer ${token}`,
+  //       'Content-Type': 'application/json',
+  //     };
+  
+  //     const response = await fetch('https://precious-tan-slug.cyclic.app/api/v2/booking/readAll', {
+  //       headers: headers,
+  //     });
+  
+  //     if (response.ok) {
+  //       const jsonData = await response.json();
+  //       setData(jsonData);
+       
+  //     } else {
+  //       console.log('Request failed with status:', response.status);
+  //     }
+  //   } catch (error) {
+  //     console.log('Error:', error);
+  //   }
+  // };
+
   return (
     <div className="driverContainer" id="booking-fix-error">
       <div className="sidebar">
@@ -23,7 +53,8 @@ const Booking = () => {
               <th className="header"> Booking Status</th>
             </thead>
             <tbody className="table-body">
-              {drivers.map((item) => (
+              {drivers ? 
+              drivers.map((item) => (
                 <tr className="table-row">
                   <td className="item0">{item.no}</td>
                   <td className="item1">{item.name}</td>
@@ -39,7 +70,7 @@ const Booking = () => {
                     </div>
                   </td>
                 </tr>
-              ))}
+              )):""}
             </tbody>
           </table>
         </div>
